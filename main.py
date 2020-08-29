016a4773e1b312d9e574df17397605e5f4dae265
Goneiross <guillaume.julien.pellerin@gmail.com>
2020-08-30 00:57:50 +0200

    Add base for TTS

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import speech_recognition as sr
 import sys
+import pyttsx3
+import os
+from playsound import playsound
 
-def analyse(text):
+def say (name, text, tts):
+    print(name + " : " + text)
+    # os.system('/opt/mimic/mimic -t ' + text + ' -voice slt' + ' -o tmp.wav')
+    # playsound('tmp.wav')
+    tts.say(text)
+    tts.runAndWait()
+
+
+def analyse(text, tts):
     if (text.find("exit") != -1 or text.find("quit") != -1):
-        print("Yumiakui: See ya space cow-boy !")
+        say("Yumiakui", "See ya space cow-boy !", tts)
         sys.exit()
     elif (text.find("hey") != -1):
-        print("Yumiakui: Aloha Gon")
+        say("Yumiakui", "Aloha Gon", tts)
     else :
-        print("Yumiakui: I don't understand this ...")
+        say("Yumiakui", "I don't understand this ...", tts)
 
 def main():
     r = sr.Recognizer()
     mic = sr.Microphone()
+    tts = pyttsx3.init()
+    tts.setProperty("rate", 180)
     while(1) :
         with mic as source:
             r.adjust_for_ambient_noise(source)
@@ -20,7 +33,7 @@ def main():
             try:
                 sentence = r.recognize_google(audio)
                 print("Goneiross: " + sentence)
-                analyse(sentence)
+                analyse(sentence, tts)
                 pass
             except:
                 print("Goneiross : ERROR Voice unrecognized")
