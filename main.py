992370fe46998ac2a78fc80d320c827d0dec3b9c
Goneiross <guillaume.julien.pellerin@gmail.com>
2020-08-29 21:10:04 +0200

    Update structure

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import speech_recognition as sr
 
-r = sr.Recognizer()
-mic = sr.Microphone()
+def main():
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+    while(1) :
+        with mic as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source)
+            try:
+                sentence = r.recognize_google(audio)
+                print("Goneiross: " + sentence)
+                pass
+            except:
+                print("Goneiross : ERROR Voice unrecognized")
+                pass
 
-while(1) :
-    with mic as source:
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source)
-        try:
-            print("Goneiross: " + r.recognize_google(audio))
-            pass
-        except:
-            print("Goneiross : ERROR Voice unrecognized")
-            pass
+if __name__ == "__main__":
+    main()
