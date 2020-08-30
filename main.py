b7adaff7f804264cd482612435048a0f5e1a7b35
Goneiross <guillaume.julien.pellerin@gmail.com>
2020-08-30 10:14:09 +0200

    Add threading to open apps

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pyttsx3
 import os
 from playsound import playsound
 import random
+from subprocess import Popen
+import threading
 
 def say (name, text, tts):
     print(name + " : " + text)
@@ -10,6 +12,28 @@ def say (name, text, tts):
     # playsound('tmp.wav')
     tts.say(text)
     tts.runAndWait()
+
+def open_code():
+    code = '/usr/share/code/bin/code'
+    Popen(code)
+
+def open_firefox():
+    firefox = '/usr/lib/firefox-trunk/firefox-trunk'
+    Popen(firefox)
+
+def open_matlab():
+    matlab = '/usr/local/MATLAB/R2020a/bin/matlab'
+    Popen(matlab)
+
+def open_Xournal():
+    xournal = '/usr/bin/xournalpp'
+    Popen(xournal)
+
+def open_spotify():
+    spotify = '/snap/bin/spotify'
+    Popen(spotify)
+
+
 
 def is_greetings(text):
     state = False
@@ -34,6 +58,18 @@ def is_leaving(text):
         "bye", 
         "see you", 
         "see ya"
+    ]
+    for word in keyword:
+        if (text.find(word) != -1):
+            state = True
+    return state
+
+def is_openning(text):
+    state = False
+    keyword = [
+        "open", 
+        "launch",
+        "start"
     ]
     for word in keyword:
         if (text.find(word) != -1):
@@ -74,10 +110,34 @@ def say_notUnderstood(tts):
     r = random.randrange(0, len(sentences))
     say("Yumiakui", sentences[r], tts)
 
+def open_app(text, tts):
+    if (text.find("code") != -1):
+        code_thread = threading.Thread(target=open_code)
+        code_thread.start()
+        say("Yumiakui", "Opening Visual Studio Code", tts)
+    elif (text.find("Firefox") != -1):
+        firefox_thread = threading.Thread(target=open_firefox)
+        firefox_thread.start()
+        say("Yumiakui", "Opening Firefox", tts)
+    elif (text.find("Matlab") != -1):
+        matlab_thread = threading.Thread(target=open_matlab)
+        matlab_thread.start()
+        say("Yumiakui", "Opening Matlab", tts)
+    elif (text.find("xournal") != -1):
+        xournal_thread = threading.Thread(target=open_xournal)
+        xournal_thread.start()
+        say("Yumiakui", "Opening Xournal", tts)
+    elif (text.find("Spotify") != -1):
+        spotify_thread = threading.Thread(target=open_spotify)
+        spotify_thread.start()
+        say("Yumiakui", "Opening Spotify", tts)
+
 def analyse(text, tts):
     if is_leaving(text):
         say_leaving(tts)
         os._exit(0)
+    elif is_openning(text):
+        open_app(text, tts)
     elif is_greetings(text):
         say_greetings(tts)
     else :
