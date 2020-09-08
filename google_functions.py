184290b2ab0bce650a4a3a1381aa09b74dde3543
Goneiross <guillaume.julien.pellerin@gmail.com>
2020-09-08 14:06:30 +0200

    Update google calandar structure

--- google_functions.py
+++ google_functions.py
@@ -6,21 +6,16 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+from google_calandar_ids import *
+
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-def calandar():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
+def google_init():
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -28,17 +23,19 @@ def calandar():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'google_calandar_api_credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
+    return creds
+
+def google_calandar():
+    creds = google_init() # SHOULD ONLY BE INIT ONCE ## TO DO
 
     service = build('calendar', 'v3', credentials=creds)
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time 
+    print('Getting the upcoming 2 events')
+    events_result = service.events().list(calendarId=CALANDAR_EPFL, timeMin=now,
+                                        maxResults=2, singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
@@ -47,5 +44,5 @@ def calandar():
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
-
+    return start
 calandar()
